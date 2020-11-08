a81c625149e71ab90749868dcf00a9691a3c45d0
Nadun Samarawickrama <nadun.samarawickrama@gmail.com>
2020-11-08 12:03:05 +0530

    Product Controller - removed unnecessary code

--- WebApplicationTest1/Controllers/ProductController.cs
+++ WebApplicationTest1/Controllers/ProductController.cs
@@ -11,46 +11,17 @@ namespace WebApplicationTest1.Controllers
         [HttpGet]
         public IActionResult GetProducts()
         {
-            int a = 10;
-            object o;
-
-            o = a;
-
-            string name = "Nadun" + "Samarawickrama";
-
-            string x = name;
-
-            string s1, s2, s3;
-            s1 = string.Empty;
-            s2 = "Nadun";
-            s3 = "Samarawickrama";
-
+            
             List<Product> list = new List<Product>();
             list.Add(new Product { id = 11, name = "IPhone", description = "Apple 6 ", unitprice = 75000.25 });
             list.Add(new Product { id = 12, name = "Samsung", description = "Galaxy", unitprice = 175000.25 });
             list.Add(new Product { id = 13, name = "OPPO", description = "Oppo V5", unitprice = 45000.25 });
             list.Add(new Product { id = 14, name = "Huwawei", description = "Huwai 5X ", unitprice = 35000.25 });
-
-            //s1 = s2 +""+ s3;
-
-            for (int i=0;i<5;i++)
-            {
-                s1 += s2;            
-            }
-
-            string temp=string.Empty;
-           
-
-            for (int i=0;i<10;i++)
-            {
-                if (temp != null)
-                {
-                    temp = temp + " ";
-                }
-            }
+                        
+            
                 
                 
-                // throw new Exception("Test");
+            // throw new Exception("Test");
             return Ok(list);
             //return StatusCode(404, errorReponse);
         }
